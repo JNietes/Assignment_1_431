f2c92fd7ce4b5deb390c6187029242c606c3eed7
Joaquin Nietes <nietesj@sunypoly.edu>
2024-09-18 10:20:10 -0400

    Completed problem 3

--- Joaquin.ml
+++ Joaquin.ml
@@ -8,11 +8,25 @@ let l3 = [3; 1; 4; 5; 9];;
 
 let rec listStatus list =
   match list with
-    | [] -> 1
-    | [hd] -> 2
-    | hd :: tl -> 0
-  ;;
+  | [] -> 1
+  | [hd] -> 2
+  | hd :: tl -> 0
+;;
 
 listStatus l1;;
 listStatus l2;;
 listStatus l3;;
+
+(* Problem 3 *)
+(* Author: Joaquin Nietes*)
+
+let rec pop list = 
+  match list with
+  | [] -> []
+  | [hd] -> [hd]
+  | hd :: tl -> pop tl
+;;
+
+pop l1;;
+pop l2;;
+pop l3;;
